01f5edc7d3efe51551a1ee71983e7f10f9c342b3
tolerantjoker <francois.roye@gmail.com>
2013-12-24 00:09:47 +0100

    Nettoyage des balises html à partir d'une url de test.

--- test2.py
+++ test2.py
@@ -3,4 +3,10 @@ Created on 23 déc. 2013
 
 @author: tolerantjoker
 '''
-print("Coucou!!!!!!!")
+from lxml import html
+from lxml.html.clean import clean_html
+
+tree = html.parse('http://www.example.com')
+tree = clean_html(tree)
+text = tree.getroot().text_content()
+print(text)
