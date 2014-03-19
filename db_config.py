26bd2311b8a459e4de98b5b3fb57c4c626bfb45d
tolerantjoker <francois.roye@gmail.com>
2014-03-19 20:23:34 +0100

    configuration de la base de données

---
+++ db_config.py
@@ -0,0 +1,12 @@
+#-*- coding: UTF-8 -*-
+'''
+Created on 19 mars 2014
+
+@author: tolerantjoker
+'''
+
+db_config = {'host': 'localhost',
+             'user': 'root',
+             'password': '',
+             'db': 'test'
+            }
