d141531736ce29487307413e47610ca10ba6159c
tolerantjoker <francois.roye@gmail.com>
2014-03-20 15:57:48 +0100

    Suppression des termes et des valeurs non porteurs de sens.

--- preprocessor.py
+++ preprocessor.py
@@ -7,9 +7,10 @@ Created on 20 mars 2014
 
 from bs4 import BeautifulSoup
 import nltk
-from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import WordPunctTokenizer
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
+import re
 
 class Preprocessor(object):
     '''
@@ -17,6 +18,7 @@ class Preprocessor(object):
     '''
     def __call__(self, html):
         raw = Preprocessor.clean_html(html)
+        raw = Preprocessor.clean_raw(raw)
         tokens = Preprocessor.tokenize(raw)
         tokens = Preprocessor.clean_stop_words(tokens)
         tokens = Preprocessor.normalize(tokens)
@@ -26,17 +28,29 @@ class Preprocessor(object):
     def clean_html(html):
         #return BeautifulSoup(html).getText()
         return nltk.clean_html(html)
+    
+    @staticmethod
+    def clean_raw(raw):
+        '''
+        Fonction qui supprime toutes les mots et valeurs non porteurs de sens
+        
+        ([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6}) --> e-mail
+        |(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/? --> url
+        |\d+ --> valeur numérique
+        |\w' --> les contractions d', l', etc.
+        |[^\w\s] --> les ponctuations 
+        '''
+        return re.sub(r'''(?x)
+        ([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})
+        |(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?
+        |\d+
+        |\w'
+        |[^\w\s]
+        ''', r'', raw)
         
     @staticmethod
     def tokenize(raw):
-        tokenizer = RegexpTokenizer(r'''(?x)
-        \d+(\.\d+)?\s*%
-        |([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})
-        |(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?
-        |\w'
-        |\w+
-        |[^\w\s]
-        ''')
+        tokenizer = WordPunctTokenizer()
         return tokenizer.tokenize(raw)
     
     @staticmethod
@@ -48,4 +62,5 @@ class Preprocessor(object):
     def normalize(tokens):
         stemmer = SnowballStemmer('french')
         return list(set([stemmer.stem(w.lower()) for w in tokens]))
+        #return list(set([w.lower() for w in tokens]))
     
