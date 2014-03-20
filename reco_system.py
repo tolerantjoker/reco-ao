051782f15e2f1c6d8c87d9b48f5cf409985f7a8f
tolerantjoker <francois.roye@gmail.com>
2014-03-20 18:29:02 +0100

    Squelette de base du système de recommandation --> 'RecoSystem'

---
+++ reco_system.py
@@ -0,0 +1,36 @@
+# -*- coding: UTF-8 -*-
+'''
+Created on 20 mars 2014
+
+@author: tolerantjoker
+'''
+
+class RecoSystem(object):
+    '''
+    classdocs
+    '''
+    
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+    
+    def get_tags_topics(self):
+        '''
+        Génère la matrice tags-topics à partir du corpus de 13000 appels d'offres
+        '''
+        pass
+    
+    def get_clients_topics(self):
+        '''
+        Génère la matrice clients-topics à partir de l'historique et de la matrice tags-topics
+        '''
+        pass
+    
+    def get_recommendation_list(self):
+        '''
+        Génère une liste de client pour un appel d'offre entrant donné.
+        '''
+        pass
+    
