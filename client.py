8fffe9da5d28c8311beaf88d15f652a6b263715f
tolerantjoker <francois.roye@gmail.com>
2014-03-23 14:16:41 +0100

    Classe client --> méthodes get_tags et get_topics()

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-#-*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 '''
 Created on 19 mars 2014
 
@@ -6,30 +6,45 @@ Created on 19 mars 2014
 '''
 
 import db_entity
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import decomposition
+import reco_system
+from preprocessor import Preprocessor
 
 class Client(object):
     '''
     classdocs
     '''
 
-    def __init__(self, id, name, url):
+    def __init__(self, params):
         '''
         Constructor
         '''
         self.dbentity = db_entity.DB_entity()
-        self.id = id
-        self.name = name
-        self.url = url
+        self.id = params['id']
+        self.name = params['name']
+        self.url = params['url']
+        
+        self.reco_sys = reco_system.RecoSystem()
+        self.historic = None
+        self.client_tags = None
+        self.client_topics = None
     
     def get_historic(self):
         with self.dbentity.conn.cursor() as cursor:
-            query ='''SELECT announce FROM assignments WHERE company = ?'''
+            query = '''SELECT announce FROM assignments WHERE company = ?'''
             params = (self.id,)
             cursor.execute(query, params)
-            return cursor.fetchall()
+            self.historic = cursor.fetchall()
+            #self.historic = [i for i in historic if i in self.reco_sys.train]
+            return self.historic
         
     def get_tags(self):
-        pass
+        vec = TfidfVectorizer(tokenizer=Preprocessor(),
+                              max_features=self.reco_sys.n_feature,
+                              vocabulary=self.reco_sys.items_tags.vocabulary_)
+        self.client_tags = vec.fit_transform(self.historic)
     
     def get_topics(self):
-        pass
+        self.client_topics = decomposition.NMF(n_components=self.reco_sys.n_components).fit(self.client_tags)
+        
