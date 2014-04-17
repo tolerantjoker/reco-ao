159a4eb93197d9fdfdbfb172cedd597f0694c89b
tolerantjoker <francois.roye@gmail.com>
2014-04-17 12:08:49 +0200

    Import du country pour élimier les appels d'offres en langue étrangère

--- import_sql.py
+++ import_sql.py
@@ -4,26 +4,34 @@ Created on 14 mars 2014
 
 @author: tolerantjoker
 '''
-## Import des appels d'offres
-from lxml import etree, etree
+# # Import des appels d'offres
+from lxml import etree
 import oursql
 
 tree = etree.parse("announces.xml")
-l_id = tree.xpath("//announce/@id")
-l_title = tree.xpath("//announce/title/text()")
-l_description = tree.xpath("//announce/description/text()")
-    
+announces = tree.xpath("//announce")
+announces_params = []
+for announce in announces:
+    announce_id = announce.xpath("@id")
+    announce_title = announce.xpath("title/text()")
+    announce_description = announce.xpath("description/text()")
+    announce_country = announce.xpath("country/text()")
+    if(announce_country == []):
+        announces_params.append((announce_id[0], announce_title[0], announce_description[0], ''))
+    else:
+        announces_params.append((announce_id[0], announce_title[0], announce_description[0], announce_country[0]))
+ 
 # Establishing a connection
 conn = oursql.connect(host='localhost', user='root', passwd='', db='test')
     
 # Using cursors
-query = '''INSERT INTO announces (id, title, description) VALUES(?,?,?)'''
-params = zip(l_id, l_title, l_description)
-#query = '''DELETE FROM announces'''
+query = '''INSERT INTO announces (id, title, description, country) VALUES(?,?,?,?)'''
+# params = zip(l_id, l_title, l_description, l_country)
+# query = '''DELETE FROM announces'''
 with conn.cursor(oursql.DictCursor) as cursor:
-    cursor.executemany(query, params)
+    cursor.executemany(query, announces_params)
 
-## Import des profils clients et des appels d'offres qui leur ont été transmis
+# # Import des profils clients et des appels d'offres qui leur ont été transmis
 
 tree = etree.parse("profiles.xml")
 profiles = tree.xpath("//company")
