3c5081e9d0bb1a9265e1e7a83f6773d1cd80e699
tolerantjoker <francois.roye@gmail.com>
2014-05-05 09:28:00 +0200

    fichier de configuration

---
+++ config.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+'''
+Created on 5 mai 2014
+
+@author: tolerantjoker
+'''
+
+SUFFIXE = '.save'
+
+VEC_RECO = 'vec_reco' + SUFFIXE
+ITEMS_TAGS = 'items_tags' + SUFFIXE
+NMF_OBJECT = 'nmf_object' + SUFFIXE
+TAGS_TOPICS = 'tags_topics' + SUFFIXE
+CLIENTS_TOPICS = 'clients_topics' + SUFFIXE
+
+RECO_DF = 'reco_df' + SUFFIXE
+
+TRAIN_SET = 'train_set' + SUFFIXE
+TEST_SET = 'test_set' + SUFFIXE
+CLIENT_TRAIN_SET = 'client_train_set'
+CLIENT_TEST_SET = 'client_test_set'
